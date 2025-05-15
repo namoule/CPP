e3d5d9a05947b2c9b2f8b11c836ba189ff14a68e
Jeanne Lefevre <jealefev@f2r6s5.clusters.42paris.fr>
2025-05-15 14:56:45 +0200

    push

--- CPP00/ex01/Contact.class.hpp
+++ CPP00/ex01/Contact.class.hpp
@@ -2,29 +2,29 @@
 #ifndef CONTACT_CLASS_HPP
 #define CONTACT_CLASS_HPP
 
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 class Contact {
     public:
-    std::string first_name;   // Prénom du contact
-    std::string last_name;    // Nom de famille du contact
-    std::string nickname;     // Surnom du contact
-    std::string phone_number; // Numéro de téléphone
-    std::string darkest_secret; // Le plus grand secret du contact
+    std::string first_name;   
+    std::string last_name;  
+    std::string nickname;   
+    std::string phone_number;
+    std::string darkest_secret; 
     
-    static const int max_contacts = 8; // Nombre maximum de contacts
+    static const int max_contacts = 8;
     static int index;
     
     Contact();
     
     ~Contact();
     
-    // Méthodes pour définir les informations du contact
     void setFirstName(const std::string &fn) { first_name = fn; }
     void setLastName(const std::string &ln) { last_name = ln; }
     void setNickname(const std::string &nm) { nickname = nm; }
-    void setPhoneNumber(const std::string &pn) { phone_number = pn; }
+    int setPhoneNumber(const std::string &pn);
     void setDarkestSecret(const std::string &ds) { darkest_secret = ds; }
     
     void displayContactInfo() const {
@@ -40,6 +40,5 @@ class Contact {
     }
 };
 
-// Initialisation du membre statique `index`
 
-#endif // CONTACT_CLASS_HPP
+#endif
--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -1,6 +1,6 @@
 #include "PhoneBook.class.hpp"
 #include "Contact.class.hpp"
-#include <iomanip>
+#include <cstring>
 int PhoneBook::index = 0;
 
 int PhoneBook::error(char *mess_err)
@@ -30,10 +30,13 @@ int PhoneBook::add()
     contacts[index].setFirstName(fn);
     contacts[index].setLastName(ln);
     contacts[index].setNickname(nm);
-    contacts[index].setPhoneNumber(pn);
+    if(contacts[index].setPhoneNumber(pn) == -1)
+    {
+        std::cout << "invalid phone number" << std::endl;
+        return(0);
+    }
     contacts[index].setDarkestSecret(ds);
     index++;
-    std::cout <<index << std::endl;
     return(0);
 }
 
@@ -61,8 +64,22 @@ int PhoneBook::search()
         std::cout << " |" << std::endl;
         i++;
     }
-
     std::cout << "--------------------------------------------------" << std::endl;
+    char val;
+    std::cout << "please enter a value between 1 and 8 (included): " << std:: endl;
+    std::cin >> val;
+    if(val <= '0' || val >= '9')
+        std::cout << "invalid value, please try again later." << std::endl;
+    else
+    {
+        int value = (val - '0') -1;
+        std::cout << "value : " << value << std::endl;
+        std::cout << "First name : [" << contacts[value].first_name << "]." << std::endl;
+        std::cout << "Last name : [" << contacts[value].last_name << "]." << std::endl;
+        std::cout << "Nickname : [" << contacts[value].nickname << "]." << std::endl;
+        std::cout << "Phone number : [" << contacts[value].phone_number << "]." << std::endl;
+        std::cout << "Darkest secret : [" << contacts[value].darkest_secret << "]." << std::endl;
+    }
     return 0;
 }
 
@@ -93,11 +110,11 @@ int PhoneBook::check_commandes(char *commande)
 int PhoneBook::command_input()
 {
     char commande[512];
-    std::cout << "Quelle est votre demande ?"<< std::endl<< "Entrez votre commande : ";
+    std::cout << "What is your request?"<< std::endl<< "Enter your order:";
     std::cin >> commande;
     if (std::cin.eof()) 
     {
-        std::cout << "\nFin de l'entrée (Ctrl+D détecté)." << std::endl;
+        std::cout << "\nEnd of entry (Ctrl+D detected)." << std::endl;
         return -42;
     }
     return(check_commandes(commande));
@@ -105,14 +122,14 @@ int PhoneBook::command_input()
 
 PhoneBook::PhoneBook(void)
 {
-    std::cout << "Bienvenue dans votre Phonebook"<< std::endl;
+    std::cout << "Welcome to your Phonebook"<< std::endl;
     while(command_input() != -42)
     exit_phonebook();
 }
 
 PhoneBook::~PhoneBook(void)
 {
-    std::cout << "Merci de votre visite !"<<std::endl;
+    std::cout << "Thank you for your visit!"<<std::endl;
 }
 
 int main(void)

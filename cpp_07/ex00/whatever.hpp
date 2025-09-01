4cae3bbfc4a363d0b4cf6f8c8fdacdd628a32709
Jeanne Lefevre <jealefev@f2r2s4.clusters.42paris.fr>
2025-09-01 13:23:27 +0200

    ex00

---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,28 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T> void swap(T &x, T &y)
+{
+    T temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+template <typename T> T min(T x, T y)
+{
+    if(x == y)
+        return(y);
+    return (x < y) ? x : y;
+}
+
+template <typename T> T max(T x, T y)
+{
+    if(x == y)
+        return(y);
+    return (x > y) ? x : y;
+}
+
+#endif

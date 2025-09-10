a543fd53e5c60fa86833260c9f8a9175c9740a52
Namoule <jeannelefevre10.22@icloud.com>
2025-09-10 19:19:25 +0200

    cp 07:

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,58 +2,27 @@
 #define ARRAY_HPP
 
 #include <exception>
+#include <cstddef>
 
 template <typename T>
-class Array {
-private:
-    T* _data;
-    unsigned int _size;
+class Array
+{
+    private:
+        T* _data;
+        unsigned int _size;
 
-public:
-    Array() : _data(nullptr), _size(0) {}
-    Array(unsigned int n) : _data(new T[n]()), _size(n) {}
-    Array(const Array& other) : _data(new T[other._size]), _size(other._size)
-    {
-        for (unsigned int i = 0; i < _size; ++i)
-            _data[i] = other._data[i];
-    }
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(const Array& other);
+        Array& operator=(const Array& other);
+        ~Array();
 
-    Array& operator=(const Array& other)
-    {
-        if (this != &other)
-        {
-            delete[] _data;
-            _size = other._size;
-            _data = new T[_size];
-            for (unsigned int i = 0; i < _size; ++i)
-                _data[i] = other._data[i];
-        }
-        return *this;
-    }
-
-    T& operator[](unsigned int index)
-    {
-        if (index >= _size)
-            throw std::exception();
-        return _data[index];
-    }
-
-    const T& operator[](unsigned int index) const
-    {
-        if (index >= _size)
-            throw std::exception();
-        return _data[index];
-    }
-
-    unsigned int size() const
-    {
-        return _size;
-    }
-
-    ~Array()
-    {
-        delete[] _data;
-    }
+        T& operator[](unsigned int index);
+        const T& operator[](unsigned int index) const;
+        unsigned int size() const;
 };
+
+#include "Array.tpp"
 
 #endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,25 @@
+#include "Array.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Array<int> a(5);
+	for (unsigned int i = 0; i < a.size(); ++i)
+		a[i] = i * 10;
+	for (unsigned int i = 0; i < a.size(); ++i)
+		std::cout << a[i] << " ";
+	std::cout << std::endl;
+	Array<int> b = a;
+	for (unsigned int i = 0; i < b.size(); ++i)
+		std::cout << b[i] << " ";
+	std::cout << std::endl;
+	try
+	{
+		std::cout << a[10] << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+	return (0);
+}
